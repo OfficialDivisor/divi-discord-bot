a2aadca758064b79f0362fc223d63c5216287102
Kaarel Virroja <kaarelvirroja@gmail.com>
2017-12-17 17:38:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,4 +11,10 @@ client.on('message', message => {
     }
 });
 
+client.on('message', message => {
+    if (message.conent === '.senddm') {
+        message.author.sendMessage("Hello, I'm just testing!");   
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
